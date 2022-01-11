c690fd51ef259cb59e1ce21bd530fe0b20bd2c46
unknown <nsaedae@gmail.com>
2022-01-11 17:51:56 +0900

    2022/01/11 수업예제

--- 4.SQL 고급실습.sql
+++ 4.SQL 고급실습.sql
@@ -87,15 +87,135 @@ INSERT INTO `Sales` (`uid`, `year`, `month`, `sale`) VALUES ('a104', 2020, 2,  8
 INSERT INTO `Sales` (`uid`, `year`, `month`, `sale`) VALUES ('a105', 2020, 2, 180000);
 INSERT INTO `Sales` (`uid`, `year`, `month`, `sale`) VALUES ('a108', 2020, 2,  76000);
 
-
-
 #실습하기 4-3
+SELECT * FROM `Member` WHERE `name` != '김춘추';
+SELECT * FROM `Member` WHERE `name` <> '김춘추';
+SELECT * FROM `Member` WHERE `pos`='사원' OR `pos`='대리';
+SELECT * FROM `Member` WHERE `pos` IN('사원', '대리');
+SELECT * FROM `Member` WHERE `name` LIKE '%신';
+SELECT * FROM `Member` WHERE `name` LIKE '정_';
+
 #실습하기 4-4
+SELECT * FROM `Sales` ORDER BY `sale`;
+SELECT * FROM `Sales` ORDER BY `sale` DESC;
+
+SELECT * FROM `Sales` 
+WHERE `sale` > 50000
+ORDER BY `year`, `month`, `sale` DESC;
+
+
 #실습하기 4-5
+SELECT * FROM `Sales` LIMIT 3;
+SELECT * FROM `Sales` LIMIT 0, 3;
+SELECT * FROM `Sales` LIMIT 4, 5;
+
+SELECT * FROM `Sales` 
+  WHERE `sale` > 50000 
+    ORDER BY `year` DESC, `month`, `sale` DESC
+      LIMIT 5;
+
 #실습하기 4-6
+SELECT SUM(`sale`) AS `합계` FROM `Sales`;
+SELECT AVG(`sale`) AS `평균` FROM `Sales`;
+SELECT COUNT(`sale`) AS `갯수` FROM `Sales`;
+SELECT COUNT(*) AS `갯수` FROM `Sales`;
+INSERT INTO `Member`
+       VALUES ('b101', '을지문덕', '010-5555-1234', '사장', 107, NOW());
+
+#확인문제1. 2018년 1월 매출의 총합을 구하시오. 
+SELECT SUM(`sale`) FROM `Sales` WHERE `year`=2018 AND `month`=1;
+
+#확인문제2. 2019년 2월 5만원 이상 매출에 대한 총합과 평균을 구하시오.
+SELECT 
+	SUM(`sale`) AS `총합`,
+	AVG(`sale`) AS `평균`
+FROM
+	`Sales`
+WHERE
+	`year`  = 2019   AND
+	`month` = 2      AND
+	`sale`  >= 50000;
+
+#확인문제3. 2020년 전체 매출 가운데 최저, 최고 매출을 구하시오.
+SELECT 
+	MIN(`sale`) AS `최저`, 
+	MAX(`sale`) AS `최고`
+FROM 
+	`Sales` 
+WHERE 
+	`year`=2020;
+
+
 #실습하기 4-7
+SELECT * FROM `Sales` GROUP BY `uid`;
+
+SELECT `uid`, SUM(`sale`) AS `합계`
+	FROM `Sales`
+	  GROUP BY `uid`;
+	  
+
 #실습하기 4-8
+SELECT `uid`, SUM(`sale`) AS `합계` FROM `Sales`
+GROUP BY `uid`
+HAVING `합계` >= 200000;
+
 #실습하기 4-9
+CREATE TABLE `Sales2` LIKE `Sales`;
+INSERT INTO `Sales2` SELECT * FROM `Sales`;
+UPDATE `Sales2` SET `year` = `year` + 3;
+UPDATE `Sales2` SET `sale` = `sale` + 1;
+
+SELECT * FROM `Sales` UNION SELECT * FROM `Sales2`;
+
 #실습하기 4-10
 #실습하기 4-11
+SELECT * FROM `Sales` JOIN `Member` ON `Sales`.`uid` = `Member`.`uid`;
+
+SELECT * FROM `Sales` AS a 
+JOIN `Member` AS b
+ON a.uid = b.uid;
+
+SELECT * FROM `Sales` AS a
+JOIN `Member` AS b
+USING(`uid`);
+
+SELECT * FROM `Sales` AS a, `Member` AS b
+WHERE a.uid = b.uid;
+
+SELECT * 
+FROM `Sales`      AS a
+JOIN `Member`     AS b ON a.uid = b.uid
+JOIN `Department` AS c ON b.dep = c.depNo;
+
+
 #실습하기 4-12
+SELECT * FROM `Sales` AS a
+RIGHT JOIN `Member` AS b
+ON a.uid = b.uid;
+
+#확인문제1. 모든직원의 아이디, 이름, 직급, 부서명을 조회하시오.
+SELECT `uid`, a.`name`, `pos`, b.`name` FROM `Member` AS a
+JOIN `Department` AS b
+ON a.dep = b.depNo;
+
+#확인문제2. '김유신' 직원의 2019년도 매출의 합을 조회하시오.
+SELECT `name`, SUM(`sale`) AS `2019년도 매출합`
+FROM `Sales` AS a
+JOIN `Member` AS b
+ON a.uid = b.uid
+WHERE `year` IN(2019) AND b.name = '김유신';
+
+
+#확인문제3. 2019년 50,000원 이상의 매출에 대해 직원별 매출의 합이 
+#           100,000원 이상인 직원의 이름, 부서명, 직급, 년도, 매출 합을 
+#           조회하시오. 단, 매출 합이 큰 순서부터 정렬
+SELECT b.`name`, c.`name`, `pos`, `year`, SUM(`sale`) AS `매출합` FROM `Sales` AS a
+JOIN `Member`     AS b ON a.uid = b.uid
+JOIN `Department` AS c ON b.dep = c.depNo
+WHERE `year` = 2019 AND `sale` >= 50000
+GROUP BY a.`uid`
+HAVING `매출합` >= 100000;
+
+
+
+
